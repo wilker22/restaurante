5f5d577496ada028a9e2b95921f533b946087196
Alex <devtamin@gmail.com>
2020-02-25 23:06:46 -0500

    create the main function page - design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,26 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <div class="row text-center">
+                        <div class="col-sm-4">
+                            <a href="/management">
+                                <h4>Managment</h4>
+                                <img width="50px" src="{{asset('images/management.svg')}}"/>
+                            </a>
+                        </div>
+                        <div class="col-sm-4">
+                            <a href="/cashier">
+                                <h4>Cashier</h4>
+                                <img width="50px" src="{{asset('images/cashier.svg')}}"/>
+                            </a>
+                        </div>
+                        <div class="col-sm-4">
+                            <a href="/report">
+                                <h4>Report</h4>
+                                <img width="50px" src="{{asset('images/report.svg')}}"/>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
