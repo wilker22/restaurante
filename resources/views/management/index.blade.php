012b5cc7a7e34ab73f5346e43dfc74ed65aa7f58
Alex <devtamin@gmail.com>
2020-02-27 17:35:58 -0500

    Create the management page

---
+++ resources/views/management/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-4">
+        <div class="list-group">
+          <a class="list-group-item list-group-item-action"><i class="fas fa-align-justify"></i> Category</a>
+          <a class="list-group-item list-group-item-action"><i class="fas fa-hamburger"></i> Menu</a>
+          <a class="list-group-item list-group-item-action"><i class="fas fa-chair"></i> Table</a>
+          <a class="list-group-item list-group-item-action"><i class="fas fa-users-cog"></i> User</a>
+        </div>
+      </div>
+      <div class="col-md-8">
+      </div>
+    </div>
+  </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/management', function(){
+    return view('management.index');
+});
