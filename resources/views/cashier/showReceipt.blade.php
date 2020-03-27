49cca25d503db807e889d60a07a7c58ccf3c5cb2
Alex <devtamin@gmail.com>
2020-03-27 00:49:14 -0400

    Show Receipt - CSS

--- resources/views/cashier/showReceipt.blade.php
+++ resources/views/cashier/showReceipt.blade.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Restuarnt App - Receipt - SaleID : {{$sale->id}}</title>
+  <link type="text/css" rel="stylesheet" href="{{asset('/css/receipt.css')}}" media="all" >
+  <link type="text/css" rel="stylesheet" href="{{asset('/css/no-print.css')}}" media="print" >
 </head>
 <body>
   <div id="wrapper">
