5b24080120dda6d0dbe7cda1dd2fe30b12a8632e
Alex <devtamin@gmail.com>
2020-03-27 20:27:31 -0400

    DateTime Picker on The Report Page

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -21,5 +21,51 @@
         </nav>
       </div>
     </div>
+    <div class="row">
+      <form action="/report/show" method="GET">
+        <div class="col-md-12">
+        <label>Choose Date For Report</label>
+          <div class="form-group">
+            <div class="input-group date" id="date-start" data-target-input="nearest">
+                  <input type="text" name="dateStart" class="form-control datetimepicker-input" data-target="#date-start"/>
+                  <div class="input-group-append" data-target="#date-start" data-toggle="datetimepicker">
+                      <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                  </div>
+              </div>
+          </div>
+
+          <div class="form-group">
+           <div class="input-group date" id="date-end" data-target-input="nearest">
+                <input type="text" name="dateEnd" class="form-control datetimepicker-input" data-target="#date-end"/>
+                <div class="input-group-append" data-target="#date-end" data-toggle="datetimepicker">
+                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                </div>
+            </div>
+          </div>
+          <input class="btn btn-primary" type="submit" value="Show Report">
+        
+        </div>
+      </form>
+    </div>
   </div>
+
+  <script type="text/javascript">
+      $(function () {
+          $('#date-start').datetimepicker({
+            format : 'L'
+          });
+          $('#date-end').datetimepicker({
+              format : 'L',
+              useCurrent: false
+          });
+          $("#date-start").on("change.datetimepicker", function (e) {
+              $('#date-end').datetimepicker('minDate', e.date);
+          });
+          $("#date-end").on("change.datetimepicker", function (e) {
+              $('#date-start').datetimepicker('maxDate', e.date);
+          });
+      });
+  </script>
+
+
 @endsection
