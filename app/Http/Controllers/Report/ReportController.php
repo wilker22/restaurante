bd98d93597fab33a2daa531c3b23f48fe92448b9
Alex <devtamin@gmail.com>
2020-03-28 02:37:15 -0400

    Show Report - Part 1 & 2

--- app/Http/Controllers/Report/ReportController.php
+++ app/Http/Controllers/Report/ReportController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers\Report;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Sale;
 
 class ReportController extends Controller
 {
     //
     public function index(){
         return view('report.index');
+    }
+    public function show(Request $request){
+        $request->validate([
+            'dateStart' => 'required',
+            'dateEnd' => 'required'
+        ]);
+        $dateStart = date("Y-m-d H:i:s", strtotime($request->dateStart.' 00:00:00'));
+        $dateEnd = date("Y-m-d H:i:s", strtotime($request->dateEnd.' 23:59:59'));
+        $sales = Sale::whereBetween('updated_at', [$dateStart, $dateEnd])->where('sale_status','paid');
+        return view('report.showReport')->with('dateStart', date("m/d/Y H:i:s", strtotime($request->dateStart.' 00:00:00')))->with('dateEnd', date("m/d/Y H:i:s", strtotime($request->dateEnd.' 23:59:59')))->with('totalSale', $sales->sum('total_price'))->with('sales', $sales->paginate(5));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,4 @@ Route::resource('management/table','Management\tableController');
 //routes for report
 
 Route::get('/report', 'Report\ReportController@index');
+Route::get('/report/show', 'Report\ReportController@show');
