c4d59afd8d8b181d790bebc29279bc959b775ae3
Alex <devtamin@gmail.com>
2020-04-01 04:24:02 -0400

    Edit a User

--- app/Http/Controllers/Management/UserController.php
+++ app/Http/Controllers/Management/UserController.php
@@ -73,7 +73,8 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = User::find($id);
+        return view('management.editUser')->with('user', $user);
     }
 
     /**
@@ -85,7 +86,20 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|min:6',
+            'role' => 'required'
+        ]);
+        $user = User::find($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->role = $request->role;
+        $user->save();
+        $request->session()->flash('status', $request->name. ' is updated successfully');
+        return redirect('/management/user');
     }
 
     /**
